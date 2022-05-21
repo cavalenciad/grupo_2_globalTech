e4e654ae1efdab7fabdae593832645b984c89f96
cavalenciad <cavalenciad@outlook.com>
2022-05-21 16:27:54 -0500

    Actualización de rutas

--- code/app.js
+++ code/app.js
@@ -13,3 +13,19 @@ app.use(express.static(publicPath));
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/index.html"));
    });
+
+app.get("/productDetail", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productDetail.html"));
+   });
+
+app.get("/login", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/login.html"));
+   });
+
+   app.get("/productCart", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/productCart.html"));
+   });
+
+   app.get("/register", (req, res) => {
+    res.sendFile(path.join(__dirname, "/views/register.html"));
+   });
