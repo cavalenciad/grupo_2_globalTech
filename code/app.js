f8819af1c0c0b2c77161f1feba0b1523e2194d92
cavalenciad <cavalenciad@outlook.com>
2022-05-21 19:51:50 -0500

    Se arregló d ó ó la navegación entre pestañas

--- code/app.js
+++ code/app.js
@@ -22,10 +22,10 @@ app.get("/login", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/login.html"));
    });
 
-   app.get("/productCart", (req, res) => {
+app.get("/productCart", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/productCart.html"));
    });
 
-   app.get("/register", (req, res) => {
+app.get("/register", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/register.html"));
    });
