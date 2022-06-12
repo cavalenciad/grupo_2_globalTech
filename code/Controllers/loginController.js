bc4ab917646ebaf3ad393fb92624de8b2e91ea99
Malekith101991 <j.gutierrezv08@gmail.com>
2022-06-12 17:26:51 -0500

    Se actualizaron archivos ejs

--- code/Controllers/loginController.js
+++ code/Controllers/loginController.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const loginController ={
     login: (req, res) => {
-        res.sendFile(path.join(__dirname, "../views/login.html"));
+        res.render("login");
 },
 };
 
--- code/routers/login.js
+++ code/routers/login.js
@@ -3,6 +3,6 @@ const router = express.Router();
 const loginController = require("../Controllers/loginController");
 
 
-router.get("/login", loginController.login);
+router.get("/", loginController.login);
 
 module.exports = router;
