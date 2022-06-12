e86ce2dd1ee1d2530d4f3145e9e5fd0d31ed78b2
cavalenciad <cavalenciad@outlook.com>
2022-06-12 17:24:37 -0500

    Actualización archivos ejs

--- code/Controllers/productDetailController.js
+++ code/Controllers/productDetailController.js
@@ -3,7 +3,7 @@ const path = require('path');
 
 const productDetailController ={
     productDetail: (req, res) => {
-        res.sendFile(path.join(__dirname, "../views/productDetail.html"));
+        res.render("productDetail");
     },
 };
 
