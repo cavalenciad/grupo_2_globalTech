6107d60ee46971b3f369deac0fdc6b4d87da1857
cavalenciad <cavalenciad@outlook.com>
2022-07-20 20:53:37 -0500

    Registro de usuario actualizado

--- code/Controllers/userController.js
+++ code/Controllers/userController.js
@@ -1,5 +1,9 @@
 const path = require('path');
+const fs = require('fs');
+const { join } = require('path');
+const { validationResult } = require('express-validator');
 
+let users = JSON.parse(fs.readFileSync(path.join(__dirname, "../data/users.json"), "utf-8"));
 
 const userController ={
     login: (req, res) => {
@@ -8,6 +12,38 @@ const userController ={
     register: (req, res) => {
         res.render("register");
     },
+    createUser: (req, res) => {
+
+        const resutlValidation = validationResult(req);
+
+        if (resutlValidation.errors.length > 0) {
+            res.render('register', {
+                errors: resutlValidation.mapped(),
+                oldData: req.body
+            });
+        }
+
+        let createUser = {
+            email: req.body.email,
+            nombre: req.body.nombre,
+            apellido: req.body.apellido,
+            nombreUser: req.body.nombreUser,
+            pais: req.body.pais,
+            imagen: req.file.filename,
+            contrasena: req.body.contrasena,
+            termCond: 'Si',
+        };
+
+        users.push(createUser);
+
+        let usersJSON = JSON.stringify(users, null, ' ');
+
+        fs.writeFileSync('./data/users.json', usersJSON,);
+
+        console.log(req.files);
+
+        res.send('Archivo subido correctamente');
+    }
 };
 
 module.exports = userController;
