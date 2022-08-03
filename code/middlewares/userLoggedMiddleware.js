95009b73971e01f2013084e56fe4c62c3ce5e69b
mqcyd <mqcyd09@gmail.com>
2022-08-02 20:33:06 -0500

    se agregan estilos a la vista de perfil

--- code/middlewares/userLoggedMiddleware.js
+++ code/middlewares/userLoggedMiddleware.js
@@ -2,11 +2,11 @@ function userLoggedMiddleware(req, res, next){
       res.locals.userLogged = false;
 
       let emailInCookie = req.cookies.userEmail;
-      let userFromCookie = User.findByField('email', emailInCookie);
+      /*let userFromCookie = User.findByField('email', emailInCookie);
 
       if(userFromCookie){
         req.session.userLogged = userFromCookie;
-      }
+      }*/
 
       if(req.session.usuarioLogueado) {
         res.locals.userLogged = true;
