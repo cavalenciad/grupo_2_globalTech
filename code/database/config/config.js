cd9ff34b153dce400a6ff57248ced587b95be0db
Sebastian-Jimenez-Mesa <sebastianjdeveloper@gmail.com>
2022-08-09 20:09:11 -0500

    Se instalan los paquetes de Sequelize y se actualiza el archivo config

---
+++ code/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "root",
+    "password": "admin",
+    "database": "db_globaltech",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": "admin",
+    "database": "db_globaltech",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": "admin",
+    "database": "db_globaltech",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
