cb7fc33fccfe24dfcbb0db547180553c6e5c9ad6
pilotrsc <teo781@hotmail.com>
2022-09-29 14:13:34 -0500

    Renovación del formulario vista login y actualice el archivo config.js para hacer el deploy

--- code/database/config/config.js
+++ code/database/config/config.js
@@ -1,23 +1,23 @@
 module.exports = {
   "development": {
-    "username": "sql10520645",
-    "password": "EhIl1JbKvM",
-    "database": "sql10520645",
+    "username": " sql10522671",
+    "password": "QbtIqexaNA",
+    "database": "sql10522671",
     "host": "sql10.freemysqlhosting.net",
     "dialect": "mysql",
     "logging": sql => sql.substr(0,20)
   },
   "test": {
-    "username": "sql10520645",
-    "password": "EhIl1JbKvM",
-    "database": "sql10520645",
+    "username": " sql10522671",
+    "password": "QbtIqexaNA",
+    "database": "sql10522671",
     "host": "sql10.freemysqlhosting.net",
     "dialect": "mysql"
   },
   "production": {
-    "username": "sql10520645",
-    "password": "EhIl1JbKvM",
-    "database": "sql10520645",
+    "username": " sql10522671",
+    "password": "QbtIqexaNA",
+    "database": "sql10522671",
     "host": "sql10.freemysqlhosting.net",
     "dialect": "mysql"
   }
