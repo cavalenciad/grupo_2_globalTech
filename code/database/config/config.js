2e9f5cc2a8af9c928fe197148b293506d6097847
cavalenciad <cavalenciad@outlook.com>
2022-10-05 21:42:08 -0500

    Actualización base de datos en Railway

--- code/database/config/config.js
+++ code/database/config/config.js
@@ -1,24 +1,27 @@
 module.exports = {
   "development": {
-    "username": "admin",
-    "password": "Juanma.1991",
-    "database": "sys",
-    "host": "database-globaltech.ccgyy5spftmr.us-east-1.rds.amazonaws.com",
+    "username": "root",
+    "port": "5506",
+    "password": "jjWgjtRTZilHTCiGGP0U",
+    "database": "railway",
+    "host": "containers-us-west-59.railway.app",
     "dialect": "mysql",
     "logging": sql => sql.substr(0,20)
   },
   "test": {
-    "username": "admin",
-    "password": "Juanma.1991",
-    "database": "sys",
-    "host": "database-globaltech.ccgyy5spftmr.us-east-1.rds.amazonaws.com",
+    "username": "root",
+    "port": "5506",
+    "password": "jjWgjtRTZilHTCiGGP0U",
+    "database": "railway",
+    "host": "containers-us-west-59.railway.app",
     "dialect": "mysql",
   },
   "production": {
-    "username": "admin",
-    "password": "Juanma.1991",
-    "database": "sys",
-    "host": "database-globaltech.ccgyy5spftmr.us-east-1.rds.amazonaws.com",
+    "username": "root",
+    "port": "5506",
+    "password": "jjWgjtRTZilHTCiGGP0U",
+    "database": "railway",
+    "host": "containers-us-west-59.railway.app",
     "dialect": "mysql",
   }
 }
