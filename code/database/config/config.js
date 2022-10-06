cf18d7f5940b4483169d8cf19816e66f2c6743c0
Malekith101991 <j.gutierrezv08@gmail.com>
2022-10-05 21:43:36 -0500

    Mergue por config

--- code/database/config/config.js
+++ code/database/config/config.js
@@ -1,24 +1,25 @@
 module.exports = {
   "development": {
     "username": "admin",
-    "password": "Juanma.1991",
-    "database": "sys",
+    "port": "3306",
+    "password": 'Juanma.1991',
+    "database": "db_globaltech",
     "host": "database-globaltech.ccgyy5spftmr.us-east-1.rds.amazonaws.com",
     "dialect": "mysql",
-    "logging": sql => sql.substr(0,20)
+
   },
   "test": {
-    "username": "admin",
-    "password": "Juanma.1991",
-    "database": "sys",
-    "host": "database-globaltech.ccgyy5spftmr.us-east-1.rds.amazonaws.com",
+    "username": "root",
+    "password": 'Juanma.1991',
+    "database": "db_globaltech",
+    "host": "localhost",
     "dialect": "mysql",
   },
   "production": {
-    "username": "admin",
-    "password": "Juanma.1991",
-    "database": "sys",
-    "host": "database-globaltech.ccgyy5spftmr.us-east-1.rds.amazonaws.com",
+    "username": "root",
+    "password": 'Juanma.1991',
+    "database": "db_globaltech",
+    "host": "localhost",
     "dialect": "mysql",
   }
 }
